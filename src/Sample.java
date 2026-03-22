c9029dae24ecd7fc6438542a95646d4a310084c3
senaelifyorucu <senaelifyorucu@gmail.com>
2026-03-22 01:15:26 +0100

    initial commit

---
+++ src/Sample.java
@@ -0,0 +1,8 @@
+public class Sample {
+    double[] features;
+    String label;
+    public Sample(double[] features, String label){
+        this.features = features;
+        this.label = label;
+    }
+}
